c0bc866bdf466f2d536f9deae07b97314332549b
Sunil <sunil_bonde@hotmail.com>
2017-12-28 10:08:02 +0000

    Added sum

--- ConsoleApplication1/ConsoleApplication1/Feature1.cs
+++ ConsoleApplication1/ConsoleApplication1/Feature1.cs
@@ -8,5 +8,12 @@ namespace ConsoleApplication1
 {
     class Feature1
     {
+        public int add()
+        {
+            int x1 = 1;
+            int x2 = 2;
+            int sum = x1 + x2;
+            return sum;
+        }
     }
 }
